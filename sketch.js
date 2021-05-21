d594051af5efa8db284abca75d6f5cef077111a9
Vaibhav Raj <77719373+vaibhavraj8252@users.noreply.github.com>
2021-05-21 22:23:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(800,800);
+ var arr= [10,20,30,40,50,60,70,80,90,100]
+  for (var i=0;i<=9;i=i+1){
+     //console.log(arr[i],i)}
+  
+  if(arr[i]%2==0){
+  console.log(arr[i])
+  }
+  for (var i=0;i>=1;i=i-1){
+    console.log(arr[i],(i))
+  }}}
+
+
+function draw(){
+  background("black");  }
+
